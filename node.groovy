d1dc7e1e8a90db51bbc0796793939d092152d299
vinodbhasyam <bhasyamvinod@gmail.com>
2018-11-29 10:46:29 +0000

    Add files via upload

---
+++ node.groovy
@@ -0,0 +1,21 @@
+job('node js example'){
+	scm{
+
+		git('git://github.com/vinodbhasyam/node-demo-app.git'){
+		node => node / gitConfigName('Vinod Bhasyam')
+				node /gitConfigEmail('bhasyamvinod@gmail.com')
+		}
+		}
+
+		triggers{
+		scm(* * * * *)
+		}
+		{
+		wrappers{
+		nodejs('nodejs')
+		}
+		steps{
+		sh ("npm install")
+		}
+		}
+}
